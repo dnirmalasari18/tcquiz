f8dcc2204067d58a3e0d11d93e09af87f6172692
Azkia <azisarf9@gmail.com>
2019-05-02 14:04:23 +0700

    Update testMhsController.php

--- app/Http/Controllers/testMhsController.php
+++ app/Http/Controllers/testMhsController.php
@@ -8,10 +8,11 @@ use App\test;
 class testMhsController extends Controller
 {
     public function index(){
-
+        $test = Test::all();
+        return view('mahasiswa.test',compact('test'));
     }
     public function store(){
-        $jawaban= new User();
+        $jawaban= new Test();
         $jawaban->jawaban_soal= $request['jawaban_soal'];
         
         $jawaban->save();

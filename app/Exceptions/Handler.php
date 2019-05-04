0d6c34984710bbf3dcb7ddcd6e25293fc8aafabe
Azkia <azisarf9@gmail.com>
2019-05-04 21:52:12 +0700

    401

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -53,6 +53,9 @@ class Handler extends ExceptionHandler
             if ($exception->getStatusCode() == 500) {
                 return response()->view('errors.' . '500', [], 500);
             }
+            if ($exception->getStatusCode() == 401) {
+                return response()->view('errors.' . '401', [], 401);
+            }
         }
         return parent::render($request, $exception);
     }

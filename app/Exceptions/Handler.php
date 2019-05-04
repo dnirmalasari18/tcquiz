c4af10d485661d5cf51b88314c27c58475efaa83
Azkia <azisarf9@gmail.com>
2019-05-04 21:48:30 +0700

    500 error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,9 @@ class Handler extends ExceptionHandler
             if ($exception->getStatusCode() == 404) {
                 return response()->view('errors.' . '404', [], 404);
             }
+            if ($exception->getStatusCode() == 500) {
+                return response()->view('errors.' . '500', [], 500);
+            }
         }
         return parent::render($request, $exception);
     }

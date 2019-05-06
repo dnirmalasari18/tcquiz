a5abf3e68bba1cbb31dba69774a01bf2d5630f67
Azkia <azisarf9@gmail.com>
2019-05-07 01:13:54 +0700

    test hidden toggle

--- resources/views/mahasiswa/test2.blade.php
+++ resources/views/mahasiswa/test2.blade.php
@@ -1,10 +1,3 @@
-@extends('layouts.mahasiswa')
-
-@section('dashboard', 'active')
-
-@section('title')
-<h1>Test</h1>
-@endsection
 
 @section('breadcrumbs')
 <li><a href="#">Dashboard</a></li>
@@ -12,7 +5,139 @@
 <li class="active">PBKK</li>
 @endsection
 
-@section('content')
+
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TCQUIZ</title>
+    <meta name="description" content="TCQUIZ">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/themify-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/selectFX/css/cs-skin-elastic.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/jqvmap/dist/jqvmap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dashboard/vendors/selectFX/css/cs-skin-elastic.css')}}">
+
+
+    <link rel="stylesheet" href="{{asset('dashboard/assets/css/style.css')}}">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+
+    <!-- Left Panel -->
+
+    <aside id="left-panel" class="left-panel toggled">
+        <nav class="navbar navbar-expand-sm navbar-default" >
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./">TCQUIZ</a>
+                <a class="navbar-brand hidden" href="./"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse" >
+                <ul class="nav navbar-nav">
+                    <li class="@yield('dashboard')">
+                        <a href="/mahasiswa"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <h3 class="menu-title">Users</h3><!-- /.menu-title -->
+                    <li class="@yield('classes')">
+                        <a href="/mahasiswa/kelas"> <i class="menu-icon fa fa-book"></i>My Classes </a>
+                    </li>
+                    <li class="@yield('quiz')">
+                        <a href="/mahasiswa/quizzes"> <i class="menu-icon fa fa-file-text"></i>Quizzes </a>
+                    </li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside><!-- /#left-panel -->
+
+    <!-- Left Panel -->
+
+    <!-- Right Panel -->
+
+    <div id="right-panel" class="right-panel">
+
+        <!-- Header-->
+        <header id="header" class="header">
+
+            <div class="header-menu">
+
+                <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                </div>
+
+                <div>
+                    <div class="user-area dropdown float-right">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+                            <div style="padding: 10px;">
+                                {{ Auth::user()->name}} {{ Auth::user()->username}} <i class="fa fa-sort-desc"></i>
+                            </div>
+                        </a>
+
+                        <div class="user-menu dropdown-menu">
+                            <a class="nav-link" href="#"><i class="fa fa-cog"></i>Change Password</a>
+                            <a class="nav-link" href="#" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i>Logout</a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header><!-- /header -->
+        <!-- Header-->
+
+        <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        @yield('title')
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            @yield('breadcrumbs')
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content mt-3">
+        
 <div class="col-md-9">
     <div class="card">
         <div class="card-header">
@@ -238,4 +363,62 @@
     </div>
 </div>
 
-@endsection
+
+        </div> <!-- .content -->
+    </div><!-- /#right-panel -->
+
+    <!-- Right Panel -->
+
+    <script src="{{asset('dashboard/vendors/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('dashboard/assets/js/main.js')}}"></script>
+
+
+    <script src="{{asset('dashboard/vendors/chart.js/dist/Chart.bundle.min.js')}}"></script>
+    <script src="{{asset('dashboard/assets/js/dashboard.js')}}"></script>
+    <script src="{{asset('dashboard/assets/js/widgets.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+
+    <script src="{{asset('dashboard/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/jszip/dist/jszip.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+    <script src="{{asset('dashboard/vendors/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
+    <script src="{{asset('dashboard/assets/js/init-scripts/data-table/datatables-init.js')}}"></script>
+    <script src="{{asset('tinymce/js/tinymce/tinymce.js')}}"></script>
+
+    <script>
+    $("#menuToggle").click(function(e) {
+        e.preventDefault();
+        $("#left-panel").toggleClass("toggled");
+    });
+
+        (function($) {
+            "use strict";
+
+            jQuery('#vmap').vectorMap({
+                map: 'world_en',
+                backgroundColor: null,
+                color: '#ffffff',
+                hoverOpacity: 0.7,
+                selectedColor: '#1de9b6',
+                enableZoom: true,
+                showTooltip: true,
+                values: sample_data,
+                scaleColors: ['#1de9b6', '#03a9f5'],
+                normalizeFunction: 'polynomial'
+            });
+        })(jQuery);
+    </script>
+
+</body>
+
+</html>

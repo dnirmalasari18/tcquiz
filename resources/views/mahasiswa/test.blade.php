71e4455a369389a305f03e52afefae80da7322fe
Azkia <azisarf9@gmail.com>
2019-05-09 20:56:11 +0700

    merge

--- resources/views/mahasiswa/test.blade.php
+++ resources/views/mahasiswa/test.blade.php
@@ -249,7 +249,6 @@
 </div>
 
 <script>
-
     function openSoal(evt, num) {
         var i, card, soal;
         card = document.getElementsByClassName("card nuzha");
@@ -280,7 +279,6 @@
             document.getElementById("nomer"+num).className = "card-body text-secondary soal-aktif";
         }
     }
-
     function flagSoal(evt, num) {
         var idFlag = "flag" + num;
         var x = document.getElementById(idFlag).checked;
@@ -293,9 +291,7 @@
             document.getElementById(id).className = "card-body text-secondary soal-aktif";
         }
     }
-
     document.getElementById("nomer1").click();
-
 </script>
 
 @endsection
@@ -308,9 +304,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 <script>
-
     $(document).ready(function(){
-
         $(".form-check-input").click(function() {
             var data = $("#myForm").serialize();
             $.ajax({
@@ -326,7 +320,6 @@
                 }
             });
         });
-
         $(".switch-input").click(function() {
             var data = $("#myForm").serialize();
             $.ajax({

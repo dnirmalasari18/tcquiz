34e73b27d7fd1b76b010ea9da9b951e4f445d5a2
Azkia <azisarf9@gmail.com>
2019-05-09 09:06:41 +0700

    Update result.blade.php

--- resources/views/mahasiswa/result.blade.php
+++ resources/views/mahasiswa/result.blade.php
@@ -18,28 +18,30 @@
                 <table class="table">
                     <tbody>
                         <tr style="border-top-style: hidden;">
-                            <td>Waktu Mulai</td>
+                            <td>Start Time</td>
                             <td>: 13:02:11</td>
                         </tr>
                         <tr>
-                            <td>Waktu Selesai</td>
+                            <td>End Time</td>
                             <td>: 14:26:08</td>
                         </tr>
                         <tr>
-                            <td>Poin</td>
+                            <td>Point</td>
                             <td>: 80.000 / 100.000 (80%)</td>
                         </tr>
                         <tr>
-                            <td>Jawaban Benar</td>
+                            <td>Correct Answers</td>
                             <td>: 16 / 20</td>
                         </tr>
                         <tr>
-                            <td>Komentar</td>
+                            <td>Comment</td>
                             <td>: -</td>
                         </tr>
                     </tbody>
+                    
                 </table>
             </div>
+            <a href="/mahasiswa" class="btn btn-info" style="float: right;"> Go Home </a>
         </div>
     </div>
 </div>

1b84a12e8a7eb20c8883ec2d4c70abf5f022c011
odeloen <odeloen.nakmus@gmail.com>
2019-05-16 09:18:18 +0700

    Adding statistics

--- resources/views/dosen/quizsummary.blade.php
+++ resources/views/dosen/quizsummary.blade.php
@@ -1,32 +1,34 @@
-<div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header bg-white">
                 <div class="row">
                     <div class="col">
-                        Persebaran Nilai
+                        <center>
+                            <h3>Statistik Quiz</h3>
+                        </center>
                     </div>
                 </div>
             </div>
             <div class="card-body">
                 <div id="persebaranNilai"></div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card-header bg-white">
-                <div class="row">
-                    <div class="col">
-                        Persebaran Jawaban No 1
+                <center>
+                    <div>
+                        <table>
+                            <tr>
+                                <td>Average</td>
+                                <td>: {!! $average !!}</td>
+                            </tr>
+                            <tr>
+                                <td>Min Score</td>
+                                <td>: {!! $min_score !!}</td>
+                            </tr>
+                            <tr>
+                                <td>Max Score</td>
+                                <td>: {!! $max_score !!}</td>
+                            </tr>                                            
+                        </table>
                     </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <div id="chart"></div>
+                </center>
             </div>
         </div>
     </div>
-</div>
